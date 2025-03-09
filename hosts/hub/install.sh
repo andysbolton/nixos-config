8407b64d83131fa42a3c236e58611b6d535f7797
Andy Bolton <andy.s.bolton@gmail.com>
2025-03-09 20:55:44 +0000

    Fix install.sh

--- hosts/hub/install.sh
+++ hosts/hub/install.sh
@@ -7,6 +7,6 @@ nixos-generate-config --no-filesystems --show-hardware-config > "$curr_dir/hardw
 sudo nix --experimental-features "nix-command flakes" \
     run 'github:nix-community/disko/latest#disko-install' -- \
     --write-efi-boot-entries \
-    --mode mount
+    --mode mount \
     --flake ~/nixos-config#hub \
     --disk main /dev/nvme0n1
