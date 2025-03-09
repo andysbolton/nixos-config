986ee73a539e88f30c87bcb1a79c43de819bf51a
Andy Bolton <andy.s.bolton@gmail.com>
2025-03-09 20:12:34 +0000

    Adding install script

---
+++ hosts/hub/install.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env bash
+
+curr_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+nixos-generate-config --no-filesystems --show-hardware-config > "$curr_dir/hardware_configuration.nix"
+
+sudo nix --experimental-features "nix-command flakes" \
+    run 'github:nix-community/disko/latest#disko-install' -- \
+    --write-efi-boot-entries \
+    --flake ~/nixos-config#hub \
+    --disk main /dev/nvme0n1
