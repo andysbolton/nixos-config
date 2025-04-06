d9733feca7efec7af0ca46313f0cb996615e0294
Andy Bolton <andy.s.bolton@gmail.com>
2025-04-05 19:24:27 -0600

    greetd: adding tuigreet, updating service settings

--- hosts/hub/configuration.nix
+++ hosts/hub/configuration.nix
@@ -129,15 +129,26 @@
     # ];
   };
 
-  # services.greetd = {
-  #  enable = true;
-  #  settings = {
-  #    default_session = {
-  #      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
-  #      user = "greeter";
-  #    };
-  #  };
-  # };
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd \"dbus-run-session river\"";
+        user = "greeter";
+      };
+    };
+  };
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
