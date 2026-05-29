1b5c9478f2cc8e6c09a70ba2c840fc68646714e8
Andy Bolton <andy.bolton@smartwyre.com>
2026-05-29 14:53:20 -0600

    sketchybar/yabai: handling removing and adding displays

--- home-manager/dotfiles/sketchybar/colors.sh
+++ home-manager/dotfiles/sketchybar/colors.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-export BASE=0xff24273a
+# Tokyo Night
+export BASE=0xff1a1b26
+export SURFACE=0xff24283b
+export OVERLAY=0xff414868
+export TEXT=0xffc0caf5
+export SUBTEXT=0xff565f89
+export BLUE=0xff7aa2f7
+export CYAN=0xff7dcfff
+export GREEN=0xff9ece6a
+export MAGENTA=0xffbb9af7
+export RED=0xfff7768e
+export YELLOW=0xffe0af68
+export ORANGE=0xffff9e64
+export TEAL=0xff73daca
+export TRANSPARENT=0x00000000
--- home-manager/dotfiles/sketchybar/plugins/battery.sh
+++ home-manager/dotfiles/sketchybar/plugins/battery.sh
@@ -9,22 +9,22 @@ fi
 
 case "${PERCENTAGE}" in
 9[0-9] | 100)
-    ICON=""
+    ICON=""
     ;;
 [6-8][0-9])
-    ICON=""
+    ICON=""
     ;;
 [3-5][0-9])
-    ICON=""
+    ICON=""
     ;;
 [1-2][0-9])
-    ICON=""
+    ICON=""
     ;;
-*) ICON="" ;;
+*) ICON="" ;;
 esac
 
 if [ "$CHARGING" != "" ]; then
-    ICON=""
+    ICON=""
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
